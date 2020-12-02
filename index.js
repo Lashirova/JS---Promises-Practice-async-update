183bddf8f7da81b0b566c23765b51a042e3e3ef4
Lachin Ashirova <lachinazra@gmail.com>
2020-12-02 05:15:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+//  require('./5-Chaining-Multiple-Promises.js');
+require('./7-promiseAll-Funciton');
+
+// require('./4-catch-Function.js');
+
+// console.log(orderPromise)
+
+// atamuradAnswer.then(handleSuccess).catch(handleFailure)
+// require('./6-Common-Mistakes.js')
